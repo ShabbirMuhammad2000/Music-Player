5fcae1680de4f17e756d346129e1d77765db21b3
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-07-27 19:33:10 -0400

    Added prev and next song functionality to buttons

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.querySelector('#title')
+const artist = document.querySelector('#artist')
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
+
+// Music
+const songs = [
+  {
+    name: 'Mubi-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Mubi',
+  },
+  {
+    name: 'Mubi-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Mubi',
+  },
+  {
+    name: 'Mubi-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Mubi',
+  },
+  {
+    name: 'Shabbir-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Mubi/Shabbir',
+  }
+];
 
 // Check if Playing
 let isPlaying = false
@@ -24,3 +51,41 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+//Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if(songIndex > songs.length - 1) {
+    songIndex = 0
+  }
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if(songIndex < 0) {
+    songIndex = songs.length - 1
+  }
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
